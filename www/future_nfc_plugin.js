8f2ea137e66a6c064c71830dc3fd9a4446650a56
futureLee1 <yksfd1064@daum.net>
2017-08-17 17:53:53 +0900

    code change

--- www/future_nfc_plugin.js
+++ www/future_nfc_plugin.js
@@ -9,9 +9,9 @@ module.exports = {
         cordova.exec(successCallback, errorCallback, "TestPlugin", "checkNfc");
     }*/
 
-    /*nfcState: function (name, successCallback, errorCallback) {*/
-    nfcState: function (name, action, successCallback, errorCallback) {
+    nfcState: function (name, successCallback, errorCallback) {
+    /*nfcState: function (name, action, successCallback, errorCallback) {*/
         /*cordova.exec(successCallback, errorCallback, "NfcPlugin", "checkNfc", [name]);*/
-        cordova.exec(successCallback, errorCallback, "NfcPlugin", [action], [name]);
+        cordova.exec(successCallback, errorCallback, "NfcPlugin", "NFC_Action", [name]);
     }
 };
